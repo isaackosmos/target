47b32193a8f4b47c1d814b5adc652f71e3bb7232
Isaac Kosmos <isaackosmos@gmail.com>
2025-08-18 11:32:24 -0300

    feat: add initial routes and layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import { router } from "expo-router";
 
 export default function Index() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Ola expo router!</Text>
+
+      <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+      <Button
+        title="Transacao"
+        onPress={() => router.navigate("/transaction/665890")}
+      />
+      <Button
+        title="Progresso"
+        onPress={() => router.navigate("/in-progress/12")}
+      />
     </View>
   );
 }
