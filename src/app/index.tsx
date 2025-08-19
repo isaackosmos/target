d421978e0fe57cdaf64ac591d798a1e8fc78bdb7
Isaac Kosmos <isaackosmos@gmail.com>
2025-08-19 16:06:58 -0300

    feat: index page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,8 @@ import { View } from "react-native";
 import { List } from "@/components/List";
 import { Target } from "@/components/Target";
 import { HomeHeader } from "@/components/HomeHeader";
+import { Button } from "@/components/Button";
+import { router } from "expo-router";
 
 const summary = {
   total: "R$ 2.680,00",
@@ -36,10 +38,19 @@ export default function Index() {
         title="Metas"
         data={targets}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Target data={item} />}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
         emptyMessage="Nenhuma meta. Toque em nova meta para criar."
         containerStyle={{ paddingHorizontal: 24 }}
       />
+
+      <View style={{ padding: 24, paddingBottom: 22 }}>
+        <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
+      </View>
     </View>
   );
 }
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,32 @@
+import {
+  ActivityIndicator,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+
+import { colors } from "@/theme";
+import { styles } from "./styles";
+
+type Props = TouchableOpacityProps & {
+  title: string;
+  isLoading?: boolean;
+};
+
+export function Button({ title, isLoading = false, ...rest }: Props) {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      activeOpacity={0.8}
+      disabled={isLoading}
+    >
+      <Text style={styles.title}>
+        {isLoading ? (
+          <ActivityIndicator size="small" color={colors.white} />
+        ) : (
+          title
+        )}
+      </Text>
+    </TouchableOpacity>
+  );
+}
