af37e9250f851ace8049a908f2bb2f31a6fe9995
Isaac Kosmos <isaackosmos@gmail.com>
2025-08-19 23:23:27 -0300

    feat: transaction interface

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,14 +1,31 @@
-import { Button, Text, View } from "react-native";
-import { router, useLocalSearchParams } from "expo-router";
+import { View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+
+import { PageHeader } from "@/components/PageHeader";
+import { CurrencyInput } from "@/components/CurrencyInput";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
 
 export default function Transaction() {
   const params = useLocalSearchParams<{ id: string }>();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>ID: {params.id}</Text>
+    <View style={{ flex: 1, padding: 24 }}>
+      <PageHeader
+        title="Nova transação"
+        subTitle="A cada valor guardado você fica mais próximo da sua meta. Se esforçe para guardar e evitar retirar."
+      />
 
-      <Button title="Voltar" onPress={() => router.back()} />
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <CurrencyInput label="Valor" value={0} />
+
+        <Input
+          label="Motivo (opcional)"
+          placeholder="Ex: Investir em CDB de 110% no banco XPTO"
+        />
+
+        <Button title="Salvar" onPress={() => {}} />
+      </View>
     </View>
   );
 }
